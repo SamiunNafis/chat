2111c82643097bc1d5447766684418de33d68c90
Samiun Nafis <luea099@gmail.com>
2021-06-13 14:21:24 +0600

    Intial commut

--- chat/global/script.js
+++ chat/global/script.js
@@ -1,23 +1,25 @@
-load()
+snapKey = ''
 
 function load() {
 
 firebase.database().ref('global/').once('value', (snapshot, s) => {
 	snapshot.forEach((ChildSnap) => {
-		console.log(ChildSnap.val().Text)
+		//console.log(ChildSnap.val().Text)
 		appendMsg(ChildSnap.val().Text, ChildSnap.val().Name)
 	})
 })
 }
 
-firebase.database().ref('global/').on('child_added', (sn, ssn) => {
-//alert(sn.val().Text)
-	appendMsg(sn.val().Text, sn.val().Name)
+firebase.database().ref('global/').on('child_added', (snapshot, sn) => {
+	snapKey = snapshot.key;
+	appendMsg(snapshot.val().Text, snapshot.val().Name, snapshot)
 })
 
-function appendMsg(text, sender) {
+function appendMsg(text, sender, snap) {
 
 let li = document.createElement('li')
+
+li.setAttribute('class', snap.key)
 
 let span = document.createElement('span')
 span.setAttribute('class', 'sender')
@@ -25,9 +27,14 @@ span.innerText = sender
 
 li.appendChild(span)
 
-li.append(text)
+li.append(text) // + ' ' + li.className)
 
 chats.appendChild(li)
+
+$('#chats').animate({
+    scrollTop: $("#chats")[0].scrollHeight
+  },
+    'fast');
 }
 
 
@@ -51,16 +58,19 @@ var mins = today.getMinutes()
 
 today = mm + '/' + dd + '/' + yyyy + ' ' + hous + ':' + mins;
 
+var myref = firebase.database().ref('global/')
+var mykey = myref.push().key
 
-firebase.database().ref('global/').push().set({
+var data = {
 	Text: txtInp,
 	Name: '',
 	Date: today
-})
+}
 
+myref.child(mykey).set(data)
 
 let li = document.createElement('li')
-li.setAttribute('class', 'me')
+li.setAttribute('class', mykey + ' me')
 li.innerText = txtInp
 
 let span = document.createElement('span')
@@ -69,5 +79,12 @@ span.innerText = 'Sender'
 li.appendChild(span)
 chats.appendChild(li)
 document.getElementById('txt').value = ''
+
+var cls = li.className.replace(' me', '')
+
+if (chats.lastChild.previousElementSibling.className == cls) {
+	chats.lastChild.previousElementSibling.style.display = 'none'
+}
+
 }
 }
